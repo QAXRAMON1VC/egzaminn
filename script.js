e9d080e85244d5032a903b43861fbc7f4caaa9d6
QAXRAMON1VC <146547491+QAXRAMON1VC@users.noreply.github.com>
2023-10-13 16:19:11 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+`function head1() {
+    document.querySelector('.hidden-section').classList.remove('sectionhid')
+}
+function head2() {
+    document.querySelector('.hidden-section').classList.add('sectionhid')
+}
+function head3() {
+    document.querySelector('.hidden-section2').classList.remove('sectionhid')
+}
+function head4() {
+    document.querySelector('.hidden-section2').classList.add('sectionhid')
+}
+function head5() {
+    document.querySelector('.hidden-section3').classList.remove('sectionhid')
+}
+function head6() {
+    document.querySelector('.hidden-section3').classList.add('sectionhid')
+}
+
+`
